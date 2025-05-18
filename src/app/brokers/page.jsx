b9882de7bb808f037addeaace8df42705b9c9519
shoaib-gillani <syedshoaibshahgillani1213@gmail.com>
2025-05-18 17:14:57 +0500

    update

--- src/app/brokers/page.jsx
+++ src/app/brokers/page.jsx
@@ -16,16 +16,19 @@ export default function Home() {
                         icon: '/svg/settings.svg',
                         title: 'Benefits of a Trusted Partnership',
                         description: 'With an experienced and dedicated broker team, we have a proven track record of delivering exceptional service.',
+                         bgColor: '#F5F2EC',
                     },
                     {
                         icon: '/svg/settings.svg',
                         title: 'Swift Closings for Your Clients',
                         description: 'Our streamlined application process and efficient underwriting allows us to provide fast and reliable funding solutions.',
+                         bgColor: '#F5F2EC',
                     },
                     {
                         icon: '/svg/settings.svg',
                         title: 'Flexible and Attractive Loan Terms',
                         description: 'We work with you to accommodate the unique needs of your clients, including customized loan structures, adjustable repayment schedules, and competitive rates.',
+                         bgColor: '#F5F2EC',
                     },
                 ]}
             />
--- src/tempates/home-page/archwest.jsx
+++ src/tempates/home-page/archwest.jsx
@@ -34,7 +34,7 @@ function Archwest({
 
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 py-8'>
           {cards.map((card, index) => (
-            <div key={index}>
+            <div key={index}  style={{ backgroundColor: card.bgColor }} className='rounded-2xl p-6'>
               <Image src={card.icon} alt={card.title} width={33} height={33} />
               <h4 className='text-[20px] font-[600] py-2'>{card.title}</h4>
               <p className='text-[#2B2B2B] text-[16px]'>{card.description}</p>
